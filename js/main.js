491d781fab48fcc8be553d2b79f03785a233144e
chernikam <bannikova.l@yandex.ru>
2023-06-19 19:12:46 +0400

    create: modal media

--- js/main.js
+++ js/main.js
@@ -1,39 +1,60 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  // Optional parameters
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    // Optional parameters
 
-  loop: true,
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: 'fade',
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: 'fade',
+  });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
 
-  loop: true,
-  slidesPerView: 1,
-  keyboard: {
-    enabled: true,
-  },
+    loop: true,
+    slidesPerView: 1,
+    keyboard: {
+      enabled: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  effect: 'slide',
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    effect: 'slide',
+  });
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function (params) {
-  console.log('Клик по кнопке меню');
-  document.querySelector('.navbar-button').classList.toggle('navbar-button--visible');
+  var menuButton = $('.menu-button');
+  menuButton.on('click', function (params) {
+    $('.navbar-button').toggleClass('navbar-button--visible');
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
 });
